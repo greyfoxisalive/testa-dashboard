351dbd7a6a39679e2e0f7ebba2e38018c1fdd3fa
grey fox <greyfoxialive@gmail.com>
2023-06-14 13:06:25 +0300

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import './index.css';
+import Editor from '@monaco-editor/react';
 
 function App() {
+  const [fileName, setFileName] = useState('script.js');
+
+  const file = files[fileName];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button disabled={fileName === 'script.js'} onClick={() => setFileName('script.js')}>
+        script.js
+      </button>
+      <button disabled={fileName === 'style.css'} onClick={() => setFileName('style.css')}>
+        style.css
+      </button>
+      <button disabled={fileName === 'index.html'} onClick={() => setFileName('index.html')}>
+        index.html
+      </button>
+      <Editor
+        height="80vh"
+        theme="vs-light"
+        path={file.name}
+        defaultLanguage={file.language}
+        defaultValue={file.value}
+      />
+    </>
   );
 }
 
+
 export default App;
+
+
+const files = {
+  'script.js': {
+    name: 'script.js',
+    language: 'javascript',
+    value: "nigga",
+  },
+  'style.css': {
+    name: 'style.css',
+    language: 'css',
+    value: "bigga",
+  },
+  'index.html': {
+    name: 'index.html',
+    language: 'html',
+    value: "zigga",
+  },
+};
